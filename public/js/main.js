b61c4ecc7e6e77096844aa9fe0348e1a1142f23f
Unknow-human <dtagboton@gmail.com>
2025-02-05 13:15:37 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -4,13 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggleThemeBtn = document.getElementById('toggleTheme');
   const themes = ['intergalactic-theme', 'sea-star-theme', 'moonlight-theme', 'cloudy-sky-theme', 'stormy-sky-theme'];
   let currentThemeIndex = 0;
-  
+
+  // Appliquer le thème stocké dans localStorage
+  const storedThemeIndex = localStorage.getItem('themeIndex');
+  if (storedThemeIndex) {
+    currentThemeIndex = parseInt(storedThemeIndex, 10);
+    document.body.classList.add(themes[currentThemeIndex]);
+  }
+
   if (toggleThemeBtn) {
     toggleThemeBtn.addEventListener('click', () => {
       document.body.classList.remove(themes[currentThemeIndex]);
       currentThemeIndex = (currentThemeIndex + 1) % themes.length;
       document.body.classList.add(themes[currentThemeIndex]);
       toggleThemeBtn.innerHTML = `Changer en ${themes[(currentThemeIndex + 1) % themes.length].replace(/-/g, ' ').replace('theme', 'thème')}`;
+      localStorage.setItem('themeIndex', currentThemeIndex);
     });
   }
 });
