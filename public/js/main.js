e05d523644e2824e628125ca890cc921f69b610c
Unknow-human <dtagboton@gmail.com>
2025-02-05 11:42:22 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,4 @@
 // public/js/main.js
-// Basculer entre le thème sombre et le thème clair
-const toggleThemeBtn = document.getElementById('toggleTheme');
-
-if (toggleThemeBtn) {
-  toggleThemeBtn.addEventListener('click', () => {
-    document.body.classList.toggle('light-theme');
-    // Sauvegarder la préférence dans le localStorage
-    if (document.body.classList.contains('light-theme')) {
-      localStorage.setItem('theme', 'light');
-    } else {
-      localStorage.setItem('theme', 'dark');
-    }
-  });
-}
-
 // Charger la préférence de thème
 window.addEventListener('load', () => {
   const theme = localStorage.getItem('theme');
@@ -23,25 +8,10 @@ window.addEventListener('load', () => {
 });
 // main.js
 
-// Fonction pour vérifier si l'utilisateur est connecté et afficher le menu
-function displayMenuIfLoggedIn() {
-  fetch('/getUserInfo')
-    .then(response => {
-      if (response.ok) {
-        const topMenu = document.getElementById('topMenu');
-        if (topMenu) {
-          topMenu.style.display = 'flex';
-        }
-      }
-    })
-    .catch(error => {
-      console.log('Utilisateur non connecté');
-    });
-}
+
 
 // Appeler la fonction au chargement de la page
 window.addEventListener('DOMContentLoaded', displayMenuIfLoggedIn);
-
 // public/js/main.js
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -53,3 +23,4 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
