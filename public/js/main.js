357883aa13ccf7b04aac7b80566ddc702ab2328b
Unknow-human <dtagboton@gmail.com>
2025-02-05 13:03:41 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,16 @@
+// public/js/main.js
+
 document.addEventListener("DOMContentLoaded", () => {
   const toggleThemeBtn = document.getElementById('toggleTheme');
+  const themes = ['intergalactic-theme', 'sea-star-theme', 'moonlight-theme', 'cloudy-sky-theme', 'stormy-sky-theme'];
+  let currentThemeIndex = 0;
   
   if (toggleThemeBtn) {
     toggleThemeBtn.addEventListener('click', () => {
-      document.body.classList.toggle('light-theme');
-      if (document.body.classList.contains('light-theme')) {
-        toggleThemeBtn.innerHTML = "Changer en Thème Sombre";
-      } else {
-        toggleThemeBtn.innerHTML = "Changer en Thème Clair";
-      }
+      document.body.classList.remove(themes[currentThemeIndex]);
+      currentThemeIndex = (currentThemeIndex + 1) % themes.length;
+      document.body.classList.add(themes[currentThemeIndex]);
+      toggleThemeBtn.innerHTML = `Changer en ${themes[(currentThemeIndex + 1) % themes.length].replace(/-/g, ' ').replace('theme', 'thème')}`;
     });
   }
 });
