14b95d9346da9ba9629c65519876168df4396397
Unknow-human <dtagboton@gmail.com>
2025-02-05 11:25:01 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -41,3 +41,15 @@ function displayMenuIfLoggedIn() {
 
 // Appeler la fonction au chargement de la page
 window.addEventListener('DOMContentLoaded', displayMenuIfLoggedIn);
+
+// public/js/main.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleThemeBtn = document.getElementById('toggleTheme');
+  
+  if (toggleThemeBtn) {
+    toggleThemeBtn.addEventListener('click', () => {
+      document.body.classList.toggle('light-theme');
+    });
+  }
+});
